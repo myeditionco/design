970c8f8e97e9275c3ce4c55d45a2980d81cdfd13
myedition <dev@my-edition.fr>
2015-11-25 22:08:20 +0100

    dark/light UI Toggle

--- components/assets/src/js/app.js
+++ components/assets/src/js/app.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
-	
+	$('#toggle-dark').change(function(){
+		$('body').toggleClass('light');
+		$(this).parent().toggleClass('white');
+		$('.container').toggleClass('white');
+	});
 });
 
